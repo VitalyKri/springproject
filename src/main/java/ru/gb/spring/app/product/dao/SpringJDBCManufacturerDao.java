c41aa610d63e11638344411026cc61374ea7de08
vitaly <VitalyKri123@gmail.com>
2022-02-12 13:07:29 +0300

    add lesson7

--- src/main/java/ru/gb/spring/app/product/dao/SpringJDBCManufacturerDao.java
+++ src/main/java/ru/gb/spring/app/product/dao/SpringJDBCManufacturerDao.java
@@ -1,81 +1,82 @@
-package ru.gb.spring.app.product.dao;
-
-import lombok.RequiredArgsConstructor;
-import ru.gb.spring.app.product.entity.Manufacturer;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
-
-//@Component
-@RequiredArgsConstructor
-public class SpringJDBCManufacturerDao implements ManufacturerDao {
-
-    private final DataSource dataSource;
-
-    @Override
-    public Manufacturer save(Manufacturer manufacturer) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Manufacturer> findAll() {
-        Connection connection = null;
-        Set<Manufacturer> result = new HashSet<>();
-
-        try {
-            connection = dataSource.getConnection();
-
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM manufacturer");
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                final Manufacturer manufacturer = Manufacturer.builder()
-                        .id(resultSet.getLong("id"))
-                        .name(resultSet.getString("name"))
-                        .build();
-                result.add(manufacturer);
-            }
-            statement.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return result;
-    }
-
-    @Override
-    public Manufacturer findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public String findNameById(Long id) {
-        return null;
-    }
-
-    @Override
-    public void insert(Manufacturer manufacturer) {
-
-    }
-
-    @Override
-    public void update(Manufacturer manufacturer) {
-
-    }
-
-    @Override
-    public void deleteById(Long id) {
-
-    }
-}
+//package ru.gb.spring.app.product.dao;
+//
+//import lombok.RequiredArgsConstructor;
+//import ru.gb.spring.app.product.entity.Manufacturer;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.HashSet;
+//import java.util.Optional;
+//import java.util.Set;
+//
+////@Component
+//@RequiredArgsConstructor
+//public class SpringJDBCManufacturerDao implements ManufacturerDao {
+//
+//    private final DataSource dataSource;
+//
+//    @Override
+//    public Manufacturer save(Manufacturer manufacturer) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Iterable<Manufacturer> findAll() {
+//        Connection connection = null;
+//        Set<Manufacturer> result = new HashSet<>();
+//
+//        try {
+//            connection = dataSource.getConnection();
+//
+//            PreparedStatement statement = connection.prepareStatement("SELECT * FROM manufacturer");
+//            ResultSet resultSet = statement.executeQuery();
+//            while (resultSet.next()) {
+//                final Manufacturer manufacturer = Manufacturer.builder()
+//                        .id(resultSet.getLong("id"))
+//                        .name(resultSet.getString("name"))
+//                        .build();
+//                result.add(manufacturer);
+//            }
+//            statement.close();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                connection.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return result;
+//    }
+//
+//    @Override
+//    public Optional<Manufacturer> findById(Long id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public String findNameById(Long id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void insert(Manufacturer manufacturer) {
+//
+//    }
+//
+//    @Override
+//    public void update(Manufacturer manufacturer) {
+//
+//    }
+//
+//    @Override
+//    public void deleteById(Long id) {
+//
+//    }
+//}
